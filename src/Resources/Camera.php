076d202ba466372f706f7dbc7467d82ccc5dd596
antonforwork <me@anton.in.ua>
2019-12-04 14:36:53 +0200

    add camera resource

---
+++ src/Resources/Camera.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace IVideon\Resources;
+
+use LazyJsonMapper\LazyJsonMapper;
+
+/**
+ * Class Camera
+ * @package IVideon\Resources
+ * @method bool getConnected();
+ * @method string getCreatedAt();
+ * @method string[] getFeatures();
+ * @method int getHeight();
+ * @method string getId();
+ * @method string getName();
+ * @method bool getOnline();
+ * @method string[] getPermissions();
+ * @method int getRotate();
+ * @method string getServer();
+ * @method int getWidth();
+ * @method bool getSoundEnabled();
+ */
+class Camera extends LazyJsonMapper
+{
+    const JSON_PROPERTY_MAP = [
+        'connected' => 'bool',
+        'created_at' => 'string',
+        'features' => 'string[]',
+        'height' => 'int',
+        'id' => 'string',
+        'mode' => 'string',
+        'name' => 'string',
+        'online' => 'bool',
+        'permissions' => 'string[]',
+        'rotate' => 'int',
+        'server' => 'string',
+        'width' => 'int',
+        'sound_enabled' => 'bool',
+    ];
+}
