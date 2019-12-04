820e9ef210c86ace28c42e6f32d0d5a61be2d531
antonforwork <me@anton.in.ua>
2019-12-04 15:22:26 +0200

    lazydoctor styler

--- src/Responses/Camera.php
+++ src/Responses/Camera.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace IVideon\Response;
+namespace IVideon\Responses;
 
 use LazyJsonMapper\LazyJsonMapper;
 
@@ -12,13 +12,67 @@ use LazyJsonMapper\LazyJsonMapper;
  * @method string[] getFeatures()
  * @method int getHeight()
  * @method string getId()
+ * @method string getMode()
  * @method string getName()
  * @method bool getOnline()
  * @method string[] getPermissions()
  * @method int getRotate()
  * @method string getServer()
- * @method int getWidth()
  * @method bool getSoundEnabled()
+ * @method int getWidth()
+ * @method bool isConnected()
+ * @method bool isCreatedAt()
+ * @method bool isFeatures()
+ * @method bool isHeight()
+ * @method bool isId()
+ * @method bool isMode()
+ * @method bool isName()
+ * @method bool isOnline()
+ * @method bool isPermissions()
+ * @method bool isRotate()
+ * @method bool isServer()
+ * @method bool isSoundEnabled()
+ * @method bool isWidth()
+ * @method $this setConnected(bool $value)
+ * @method $this setCreatedAt(string $value)
+ * @method $this setFeatures(string[] $value)
+ * @method $this setHeight(int $value)
+ * @method $this setId(string $value)
+ * @method $this setMode(string $value)
+ * @method $this setName(string $value)
+ * @method $this setOnline(bool $value)
+ * @method $this setPermissions(string[] $value)
+ * @method $this setRotate(int $value)
+ * @method $this setServer(string $value)
+ * @method $this setSoundEnabled(bool $value)
+ * @method $this setWidth(int $value)
+ * @method $this unsetConnected()
+ * @method $this unsetCreatedAt()
+ * @method $this unsetFeatures()
+ * @method $this unsetHeight()
+ * @method $this unsetId()
+ * @method $this unsetMode()
+ * @method $this unsetName()
+ * @method $this unsetOnline()
+ * @method $this unsetPermissions()
+ * @method $this unsetRotate()
+ * @method $this unsetServer()
+ * @method $this unsetSoundEnabled()
+ * @method $this unsetWidth()
+ *
+ * @property bool $connected
+ * @property string $created_at
+ * @property string[] $features
+ * @property int $height
+ * @property string $id
+ * @property string $mode
+ * @property string $name
+ * @property bool $online
+ * @property string[] $permissions
+ * @property int $rotate
+ * @property string $server
+ * @property bool $sound_enabled
+ * @property int $width
  */
 class Camera extends LazyJsonMapper
 {
--- src/Responses/Server.php
+++ src/Responses/Server.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace IVideon\Response;
+namespace IVideon\Responses;
 
 use LazyJsonMapper\LazyJsonMapper;
 
@@ -9,9 +9,9 @@ use LazyJsonMapper\LazyJsonMapper;
  *
  * @method Camera[] getCameras()
  * @method bool getConnected()
- * @method string getId()
  * @method string getCreatedAt()
  * @method string[] getFeatures()
+ * @method string getId()
  * @method string getMacAddress()
  * @method string getName()
  * @method string getNetworkType()
@@ -19,11 +19,60 @@ use LazyJsonMapper\LazyJsonMapper;
  * @method string getOwner()
  * @method string getOwnerName()
  * @method string getSoftwareVersion()
+ * @method bool isCameras()
+ * @method bool isConnected()
+ * @method bool isCreatedAt()
+ * @method bool isFeatures()
+ * @method bool isId()
+ * @method bool isMacAddress()
+ * @method bool isName()
+ * @method bool isNetworkType()
+ * @method bool isOnline()
+ * @method bool isOwner()
+ * @method bool isOwnerName()
+ * @method bool isSoftwareVersion()
+ * @method $this setCameras(Camera[] $value)
+ * @method $this setConnected(bool $value)
+ * @method $this setCreatedAt(string $value)
+ * @method $this setFeatures(string[] $value)
+ * @method $this setId(string $value)
+ * @method $this setMacAddress(string $value)
+ * @method $this setName(string $value)
+ * @method $this setNetworkType(string $value)
+ * @method $this setOnline(bool $value)
+ * @method $this setOwner(string $value)
+ * @method $this setOwnerName(string $value)
+ * @method $this setSoftwareVersion(string $value)
+ * @method $this unsetCameras()
+ * @method $this unsetConnected()
+ * @method $this unsetCreatedAt()
+ * @method $this unsetFeatures()
+ * @method $this unsetId()
+ * @method $this unsetMacAddress()
+ * @method $this unsetName()
+ * @method $this unsetNetworkType()
+ * @method $this unsetOnline()
+ * @method $this unsetOwner()
+ * @method $this unsetOwnerName()
+ * @method $this unsetSoftwareVersion()
+ *
+ * @property Camera[] $cameras
+ * @property bool $connected
+ * @property string $created_at
+ * @property string[] $features
+ * @property string $id
+ * @property string $mac_address
+ * @property string $name
+ * @property string $network_type
+ * @property bool $online
+ * @property string $owner
+ * @property string $owner_name
+ * @property string $software_version
  */
 class Server extends LazyJsonMapper
 {
     const JSON_PROPERTY_MAP = [
-        'cameras'          => 'Resources\\Cameras[]',
+        'cameras'          => 'Camera[]',
         'connected'        => 'bool',
         'id'               => 'string',
         'created_at'       => 'string',
